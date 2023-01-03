d4f810f0d3f29c291558d42c1c572aae5fbdab8d
rathanak1 <bunrattanak44@gmail.com>
2023-01-03 16:21:59 +0700

    New code

---
+++ index.php
@@ -0,0 +1,88 @@
+<?php
+print("Hello World<br>");
+echo "Hello World";
+printf("Hello World". "<br>"); 
+Printf("i'm Rathanak". "<br>")
+?>
+
+<!-- Variables in PHP -->
+
+<?php
+$intNum = 472;
+$doubleNum = 29.3;
+$boolean = true;
+$string = "Web Code Geeks";
+$array = array("Pineapple", "Grapefruit", "Banana");
+
+echo "My data : " . $boolean ."<br>";
+echo "My data array : " . $array[0];
+?>
+<!-- Conditional Statements in PHP -->
+<br>
+<?php
+$age = 18;
+if ($age < 20) {
+echo "You are a teenager";
+}
+?>
+<br>
+<!-- The If. . . Else statement -->
+<?php
+$age = 25;
+if ($age < 20) {
+echo "You are a teenager";
+}else {
+echo "You are an adult";
+}
+?>
+<br>
+<!-- The If. . . Elseif. . . Else statement -->
+<?php
+$age = 3;
+if ($age < 10) {
+echo "You are a kid";
+} elseif ($age < 20) {
+echo "You are a teenager";
+} else {
+echo "You are an adult";
+}
+?>
+<br>
+<!-- Loops in PHP -->
+<?php
+for ($i=0; $i < 5; $i++) {
+    echo "This is loop number $i". "<br>";
+    }
+    
+?>
+<br>
+<!-- The while loop -->
+<?php
+$i=0; // initialization
+while ($i < 5) {
+echo "This is loop number $i". "<br>";
+$i++; // step
+}
+?>
+<br>
+<!-- The do. . . while loop -->
+<?php
+$i = 0; // initialization
+do {
+$i++; // step
+echo "This is loop number $i". "<br>";
+}while ($i < 5); // condition
+?>
+<br>
+<!-- The foreach loop -->
+<?php
+$var = array("a","b","c","d","e");
+// array declaration
+foreach ($var as $key) {
+echo "Element is $key". "<br>";
+}
+?>
+
+<?php
+
+?>
---
+++ variable.php
@@ -0,0 +1,9 @@
+<?php
+$intNum = 472;
+$doubleNum = 29.3;
+$boolean = true;
+$string = "Web Code Geeks";
+$array = array("Pineapple", "Grapefruit", "Banana");
+echo "My data:" . $intNum. " <\br>" ;
+echo "My data array:";
+?>
